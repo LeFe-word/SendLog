0bd893d10ee128223a798abf82332464c9fe85b1
LeFe-word <142906595+LeFe-word@users.noreply.github.com>
2025-01-14 13:04:30 +0300

    Create inst_sender.sh

---
+++ inst_sender.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install -y python3 python3-pip
+python3 --version
+sleep 2
+pip3 install requests
+
+apt install cron
+sudo systemctl start cron
+sudo systemctl enable cron
+
