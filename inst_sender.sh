ab2d3236160b51028a73be835b11ea97740e0e06
LeFe-word <142906595+LeFe-word@users.noreply.github.com>
2025-01-14 15:31:40 +0300

    Update inst_sender.sh

--- inst_sender.sh
+++ inst_sender.sh
@@ -9,7 +9,16 @@ apt install cron
 sudo systemctl start cron
 sudo systemctl enable cron
 
-wget 
+wget https://raw.githubusercontent.com/LeFe-word/SendLog/main/send_logs.py
+wget https://raw.githubusercontent.com/LeFe-word/SendLog/main/send_log.sh
+
+echo "Please enter bot token:"
+read TOKEN
+echo "Chat_id:"
+read CHAT_ID
+
+echo "python3 send_logs.py --TOKEN $TOKEN --CHAT_ID $CHAT_ID"
+chmod +x send_log.sh
 
 
 
